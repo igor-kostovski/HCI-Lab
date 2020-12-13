fd795f17d5304ad3eb4a0ddb49a95df0a9719d67
LukaPodrug <lukapodra1@gmail.com>
2020-12-13 23:50:26 +0100

    closing burger menu on outside click and select added

--- HCI-lab/src/components/burgerMenu/index.js
+++ HCI-lab/src/components/burgerMenu/index.js
@@ -2,17 +2,39 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { slide as Menu } from "react-burger-menu"
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import { navigationTabs } from "../../constants"
 
-const BurgerMenu = () => (   
-    <Menu right>
-        {navigationTabs.map(({name, linkTo}) => (
-            <Link to={linkTo} key={name}>
-                <div>{name.toUpperCase()}</div>
-            </Link>
-        ))}
-    </Menu>
-)
+class BurgerMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    handleStateChange(state) {
+        this.setState({menuOpen: state.isOpen})  
+    }
+
+    closeMenu () {
+        this.setState({menuOpen: false})
+    }
+
+    render() {
+        return (
+            <OutsideClickHandler onOutsideClick={() => this.closeMenu()}>
+                <Menu isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)} right>
+                    {navigationTabs.map(({name, linkTo}) => (
+                        <Link onClick={() => this.closeMenu()} to={linkTo} key={name}>
+                            <div>{name.toUpperCase()}</div>
+                        </Link>
+                    ))}
+                </Menu>
+            </OutsideClickHandler>
+        )
+    }
+}
 
 export default BurgerMenu

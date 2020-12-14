fcc2c797114b8ee9124b2afe249f26c799bbbeed
igor <igor.kostovski98@gmail.com>
2020-12-14 19:15:28 +0100

    rewritting BurgerMenu component to support React Hooks

--- HCI-lab/src/components/burgerMenu/index.js
+++ HCI-lab/src/components/burgerMenu/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import { slide as Menu } from "react-burger-menu"
@@ -6,35 +6,30 @@ import OutsideClickHandler from 'react-outside-click-handler';
 
 import { navigationTabs } from "../../constants"
 
-class BurgerMenu extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            menuOpen: false
-        }
+
+const BurgerMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleStateChange = ({ isOpen }) => {
+        setIsOpen(isOpen);
     }
 
-    handleStateChange(state) {
-        this.setState({menuOpen: state.isOpen})  
+    const closeMenu = () => {
+        setIsOpen(false);
     }
 
-    closeMenu () {
-        this.setState({menuOpen: false})
-    }
-
-    render() {
-        return (
-            <OutsideClickHandler onOutsideClick={() => this.closeMenu()}>
-                <Menu isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)} right>
-                    {navigationTabs.map(({name, linkTo}) => (
-                        <Link onClick={() => this.closeMenu()} to={linkTo} key={name}>
-                            <div>{name.toUpperCase()}</div>
-                        </Link>
-                    ))}
-                </Menu>
-            </OutsideClickHandler>
-        )
-    }
+    return (
+        <OutsideClickHandler onOutsideClick={() => closeMenu()}>
+            <Menu isOpen={isOpen} onStateChange={(menuState) => handleStateChange(menuState)} right>
+                {navigationTabs.map(({ name, linkTo }) => (
+                    <Link onClick={() => closeMenu()} to={linkTo} key={name}>
+                        <div>{name.toUpperCase()}</div>
+                    </Link>
+                ))}
+            </Menu>
+        </OutsideClickHandler>
+    );
 }
 
 export default BurgerMenu

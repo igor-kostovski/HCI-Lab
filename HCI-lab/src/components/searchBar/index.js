e5b7e617af3a0f642983c840e84e6d09519b0aa1
igor <igor.kostovski98@gmail.com>
2020-12-20 13:16:41 +0100

    adding search css and basic action impl

--- HCI-lab/src/components/searchBar/index.js
+++ HCI-lab/src/components/searchBar/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './style.module.css'
 
-const SearchBar = () => {
-    return (<></>);
+const onEnter = ({ charCode, target: { value } }, searchAction) => {
+    if (charCode === 13) {
+        searchAction(value);
+    }
+}
+
+const SearchBar = ({ action }) => {
+    return (
+        <div className={styles.searchContainer}>
+            <input className={styles.searchInput}
+                type="text"
+                placeholder="Search"
+                onKeyPress={(event) => onEnter(event, action)} />
+        </div>
+    );
+}
+
+SearchBar.propTypes = {
+    action: PropTypes.func.isRequired
 }
 
 export default SearchBar;
--- HCI-lab/src/pages/blog.js
+++ HCI-lab/src/pages/blog.js
@@ -1,8 +1,12 @@
 import React from "react"
+import SearchBar from "../components/searchBar";
+
+const onSearchAction = (value) => {
+  console.log('searching for ' + value);
+}
 
 const BlogPage = () => (
-  <>
-  </>
+  <SearchBar action={onSearchAction} />
 )
 
 export default BlogPage

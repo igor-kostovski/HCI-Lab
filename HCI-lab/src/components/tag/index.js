3cba88bb11a4f01c8688626aa5fe41687375201b
igor <igor.kostovski98@gmail.com>
2020-12-23 19:52:37 +0100

    adding tag component

--- HCI-lab/src/components/tag/index.js
+++ HCI-lab/src/components/tag/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tag = () => {
-    return (<></>);
+import styles from './style.module.css'
+
+const Tag = ({ title, action }) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleOnClick = () => {
+        setIsClicked(!isClicked);
+        action();
+    }
+
+    const getStyle = () => {
+        let style = `${styles.btn}`;
+
+        if (isClicked) {
+            style += ` ${styles.clicked}`;
+        } else {
+            style += ` ${styles.effect}`;
+        }
+
+        return style;
+    }
+
+    return (
+        <div className={styles.buttons}>
+            <button onClick={handleOnClick} className={getStyle()}>
+                {title}
+            </button>
+        </div>
+    );
 }
 
 export default Tag;
--- HCI-lab/src/pages/blog.js
+++ HCI-lab/src/pages/blog.js
@@ -1,15 +1,20 @@
 import React from "react"
 import SearchBar from "../components/searchBar";
+import Layout from "../layouts"
+import Tag from "../components/tag";
 
 const onSearchAction = (value) => {
   console.log('searching for ' + value);
 }
 
-import Layout from "../layouts"
+const onTagAction = () => {
+  console.log('clicked tag');
+}
 
 const BlogPage = () => (
   <Layout>
     <SearchBar action={onSearchAction} />
+    <Tag title={'Tag'} action={onTagAction} />
   </Layout>
 )
 

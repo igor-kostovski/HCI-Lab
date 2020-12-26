ed49cb18bc008e8724a4241bd72e7b208c54aca8
igor <igor.kostovski98@gmail.com>
2020-12-26 14:44:56 +0100

    code cleanup

--- HCI-lab/src/components/burgerMenu/index.js
+++ HCI-lab/src/components/burgerMenu/index.js
@@ -5,6 +5,7 @@ import { slide as Menu } from "react-burger-menu"
 import OutsideClickHandler from 'react-outside-click-handler';
 
 import { navigationTabs } from "../../constants"
+
 import styles from "./style.css"
 
 const BurgerMenu = () => {

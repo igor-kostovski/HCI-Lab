4cdc03a5159aeff3ab790461546c464d4cf1307b
igor <igor.kostovski98@gmail.com>
2020-12-30 12:13:46 +0100

    adding initial specs page layout

--- HCI-lab/src/pages/specs.js
+++ HCI-lab/src/pages/specs.js
@@ -1,8 +1,36 @@
 import React from "react"
 
+import Card from '../components/card';
+import { cardType, images } from "../constants";
+import styles from './specs.module.css'
+
 const SpecsPage = () => (
-  <>
-  </>
+  <div className={styles.container}>
+    <div className={styles.gridItem1}>
+      <Card header='Technical info'
+        image={images.logo}
+        cardType={cardType.specs}
+        text='test' />
+    </div>
+    <div className={styles.gridItem2}>
+      <Card header='Accomodation'
+        image={images.logo}
+        cardType={cardType.specs}
+        text='test' />
+    </div>
+    <div className={styles.gridItem3}>
+      <Card header='Onboard entertainment'
+        image={images.logo}
+        cardType={cardType.specs}
+        text='test' />
+    </div>
+    <div className={styles.gridItem4}>
+      <Card header='Water sports'
+        image={images.logo}
+        cardType={cardType.specs}
+        text='test' />
+    </div>
+  </div>
 )
 
 export default SpecsPage

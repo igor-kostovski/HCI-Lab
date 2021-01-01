ffa05dc2ba2b128f4e920c195a126f63d96e7a60
LukaPodrug <lukapodra1@gmail.com>
2021-01-01 17:38:15 +0100

    added ciruclar gallery layout

--- HCI-lab/src/pages/gallery.js
+++ HCI-lab/src/pages/gallery.js
@@ -1,7 +1,31 @@
 import React from "react"
 
+import styles from "./gallery.module.css"
+
 const GalleryPage = () => (
   <>
+    <div className={styles.galleryContainer}>
+        <div className={styles.outerCircleLine}>
+        </div>
+        <div className={styles.centerCircle}>
+        </div>
+        <div className={styles.topImageCircle}>
+        </div>
+        <div className={styles.bottomImageCircle}>
+        </div>
+        <div className={styles.rightImageCircle}>
+        </div>
+        <div className={styles.leftImageCircle}>
+        </div>
+        <div className={styles.topRightImageCircle}>
+        </div>
+        <div className={styles.topLeftImageCircle}>
+        </div>
+        <div className={styles.bottomRightImageCircle}>
+        </div>
+        <div className={styles.bottomLeftImageCircle}>
+        </div>
+    </div>
   </>
 )
 

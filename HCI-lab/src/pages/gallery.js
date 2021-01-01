4d451fc17476b1436a9a873f97bccfa0a6eeaf04
LukaPodrug <lukapodra1@gmail.com>
2021-01-01 20:00:58 +0100

    added gallery slider layout

--- HCI-lab/src/pages/gallery.js
+++ HCI-lab/src/pages/gallery.js
@@ -5,10 +5,10 @@ import styles from "./gallery.module.css"
 const GalleryPage = () => (
   <>
     <div className={styles.galleryContainer}>
+      <div className={styles.galleryLeftSlide}>
         <div className={styles.outerCircleLine}>
         </div>
-        <div className={styles.centerCircle}>
-        </div>
+        <div className={styles.centerCircle}>EXTERIOR</div>
         <div className={styles.topImageCircle}>
         </div>
         <div className={styles.bottomImageCircle}>
@@ -27,6 +27,53 @@ const GalleryPage = () => (
         </div>
         <div className={styles.rightArrowEnabled}/>
         <div className={styles.leftArrowDisabled}/>
+      </div>
+      <div className={styles.galleryMainSlide}>
+        <div className={styles.outerCircleLine}>
+        </div>
+        <div className={styles.centerCircle}>INTERIOR</div>
+        <div className={styles.topImageCircle}>
+        </div>
+        <div className={styles.bottomImageCircle}>
+        </div>
+        <div className={styles.rightImageCircle}>
+        </div>
+        <div className={styles.leftImageCircle}>
+        </div>
+        <div className={styles.topRightImageCircle}>
+        </div>
+        <div className={styles.topLeftImageCircle}>
+        </div>
+        <div className={styles.bottomRightImageCircle}>
+        </div>
+        <div className={styles.bottomLeftImageCircle}>
+        </div>
+        <div className={styles.rightArrowEnabled}/>
+        <div className={styles.leftArrowEnabled}/>
+      </div>
+      <div className={styles.galleryRightSlide}>
+        <div className={styles.outerCircleLine}>
+        </div>
+        <div className={styles.centerCircle}>MAIN DECK</div>
+        <div className={styles.topImageCircle}>
+        </div>
+        <div className={styles.bottomImageCircle}>
+        </div>
+        <div className={styles.rightImageCircle}>
+        </div>
+        <div className={styles.leftImageCircle}>
+        </div>
+        <div className={styles.topRightImageCircle}>
+        </div>
+        <div className={styles.topLeftImageCircle}>
+        </div>
+        <div className={styles.bottomRightImageCircle}>
+        </div>
+        <div className={styles.bottomLeftImageCircle}>
+        </div>
+        <div className={styles.rightArrowDisabled}/>
+        <div className={styles.leftArrowEnabled}/>
+      </div>
     </div>
   </>
 )

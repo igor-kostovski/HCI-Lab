97edeba9b3c9e6af93bcacb5f63ea20e3b5875e8
igor <igor.kostovski98@gmail.com>
2021-01-02 12:52:13 +0100

    refactoring card component

--- HCI-lab/src/components/card/index.js
+++ HCI-lab/src/components/card/index.js
@@ -1,85 +1,44 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import blogStyle from "./blog.module.css"
-import crewStyle from "./crew.module.css"
-import infoStyle from "./info.module.css"
-import specsStyle from "./specs.module.css"
+import styles from './style.module.css';
 import Image from '../image';
-import { cardType as cardTypes, crewInfo, buttonTexts } from '../../constants';
+import { crewInfo, buttonTexts } from '../../constants';
 
-const getStylesFor = (card) => {
-    switch (card) {
-        case cardTypes.blog:
-            return blogStyle;
-
-        case cardTypes.crew:
-            return crewStyle;
-
-        case cardTypes.info:
-            return infoStyle;
-
-        case cardTypes.specs:
-            return specsStyle;
-
-        default:
-            return crewStyle;
-    }
-}
-
-const Card = ({ header, text, image, cardType, shortBio }) => {
-    const [isExpanded, setIsExpanded] = useState(false)
-
-    const onClickMoreInfo = () => {
-        setIsExpanded(!isExpanded);
-    }
-
-    const styles = getStylesFor(cardType);
+const Card = ({ header, text, image, shortBio }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
 
     return (
         <div className={styles.card}>
-            {cardType!==cardTypes.crew && <>
-                                            {image && <Image className={styles.image} name={image} />}
-                                            <div className={styles.container}>
-                                                <h4 className={styles.header}>
-                                                    {header}
-                                                </h4>
-                                                <p className={styles.text}>{text}</p>
-                                            </div>
-                                          </>
-            }
-            {cardType===cardTypes.crew && <> 
-                                            <div className={styles.contentContainer}>
-                                                <div className={isExpanded ? styles.imageTitleTextMoved : styles.imageTitleText}>
-                                                    {image && <Image className={styles.image} name={image} />}
-                                                    <div className={styles.container}>
-                                                        <h4 className={styles.header}>
-                                                            {header}
-                                                        </h4>
-                                                        {shortBio && <ul className={styles.shortBio}>
-                                                                         <li>{crewInfo.labels.dateOfBirth} {shortBio.dateOfBirth}</li>
-                                                                         <li>{crewInfo.labels.languages} {shortBio.languages}</li>
-                                                                         <li>{crewInfo.labels.experience} {shortBio.experience}</li>
-                                                                     </ul>
-                                                        }
-                                                    </div>
-                                                </div>
-                                                <div className={isExpanded ? styles.moreInfoContainerShown : styles.moreInfoContainerHidden}>
-                                                    <div className={styles.moreInfoTitle}>
-                                                        {header}
-                                                    </div> 
-                                                    <div className={styles.moreInfoText}>
-                                                        {text}
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className={styles.moreInfoButton}>
-                                                <div className={isExpanded ? styles.hideInfoButtonText : styles.moreInfoButtonText} onClick={() => onClickMoreInfo()}>
-                                                    {isExpanded ? buttonTexts.hide : buttonTexts.show}
-                                                </div>
-                                            </div>
-                                          </>
-            }
+            <div className={styles.contentContainer}>
+                <div className={isExpanded ? styles.imageTitleTextMoved : styles.imageTitleText}>
+                    <Image className={styles.image} name={image} />
+                    <div className={styles.container}>
+                        <h4 className={styles.header}>
+                            {header}
+                        </h4>
+                        {shortBio && <ul className={styles.shortBio}>
+                            <li>{crewInfo.labels.dateOfBirth} {shortBio.dateOfBirth}</li>
+                            <li>{crewInfo.labels.languages} {shortBio.languages}</li>
+                            <li>{crewInfo.labels.experience} {shortBio.experience}</li>
+                        </ul>
+                        }
+                    </div>
+                </div>
+                <div className={isExpanded ? styles.moreInfoContainerShown : styles.moreInfoContainerHidden}>
+                    <div className={styles.moreInfoTitle}>
+                        {header}
+                    </div>
+                    <div className={styles.moreInfoText}>
+                        {text}
+                    </div>
+                </div>
+            </div>
+            <div className={styles.moreInfoButton}>
+                <div className={isExpanded ? styles.hideInfoButtonText : styles.moreInfoButtonText} onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? buttonTexts.hide : buttonTexts.show}
+                </div>
+            </div>
         </div>
     );
 }
@@ -87,8 +46,7 @@ const Card = ({ header, text, image, cardType, shortBio }) => {
 Card.propTypes = {
     header: PropTypes.string,
     text: PropTypes.string.isRequired,
-    image: PropTypes.string,
-    cardType: PropTypes.number,
+    image: PropTypes.string.isRequired,
     shortBio: PropTypes.object
 }
 

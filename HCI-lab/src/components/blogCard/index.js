7909a3728a5cc408fb62ddd958b6a6b860517b2f
LukaPodrug <lukapodra1@gmail.com>
2021-01-13 16:58:29 +0100

    added blog responsivity

--- HCI-lab/src/components/blogCard/index.js
+++ HCI-lab/src/components/blogCard/index.js
@@ -6,27 +6,29 @@ import Image from '../image';
 
 const BlogCard = ({ post, imageName }) => {
     return (
-        <div className={`${styles.blogCard} ${styles.springFever}`}>
-            <Image isBackground={true} name={imageName} />
-            <div className={styles.titleContent}>
-                <h3>{post.title}</h3>
+        <a href={post.link} target='blank'>
+            <div className={`${styles.blogCard} ${styles.springFever}`}>
+                <Image isBackground={true} name={imageName} />
+                <div className={styles.titleContent}>
+                    <h3>{post.title}</h3>
+                </div>
+                <div className={styles.cardInfo}>
+                    {post.text}
+                    <a href={post.link} target='blank'>
+                        Read Article
+                        <span className={`${styles.licon} ${styles.iconArr} ${styles.iconBlack}`}></span>
+                    </a>
+                </div>
+                <div className={styles.utilityInfo}>
+                    <ul className={styles.utilityList}>
+                        <li><span className={`${styles.licon} ${styles.iconDat}`}></span>{post.date}</li>
+                        <li><span className={`${styles.licon} ${styles.iconTag}`}></span>{post.tags.join(', ')}</li>
+                    </ul>
+                </div>
+                <div className={styles.gradientOverlay}></div>
+                <div className={styles.colorOverlay}></div>
             </div>
-            <div className={styles.cardInfo}>
-                {post.text}
-                <a href={post.link} target='blank'>
-                    Read Article
-                    <span className={`${styles.licon} ${styles.iconArr} ${styles.iconBlack}`}></span>
-                </a>
-            </div>
-            <div className={styles.utilityInfo}>
-                <ul className={styles.utilityList}>
-                    <li><span className={`${styles.licon} ${styles.iconDat}`}></span>{post.date}</li>
-                    <li><span className={`${styles.licon} ${styles.iconTag}`}></span>{post.tags.join(', ')}</li>
-                </ul>
-            </div>
-            <div className={styles.gradientOverlay}></div>
-            <div className={styles.colorOverlay}></div>
-        </div>
+        </a>
     );
 }
 

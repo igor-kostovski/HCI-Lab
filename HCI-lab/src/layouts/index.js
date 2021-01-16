92fb6ad1bf5178e2d3e11e359303f0cb34fa8fd4
igor <igor.kostovski98@gmail.com>
2021-01-16 21:22:42 +0100

    adding layout color

--- HCI-lab/src/layouts/index.js
+++ HCI-lab/src/layouts/index.js
@@ -4,6 +4,7 @@ import { navigationContext } from '../constants/contexts';
 import Header from "../components/header"
 import Footer from "../components/footer"
 import PageTitle from "../components/pageTitle"
+import styles from './style.module.css'
 
 const Layout = ({ children }) => {
     var { activeTab, setActiveTab } = useContext(navigationContext);
@@ -13,7 +14,7 @@ const Layout = ({ children }) => {
         else if (string === '') return 'Home'
         else if (string === 'aboutUs') return 'About us'
         return string.charAt(0).toUpperCase() + string.slice(1)
-      }
+    }
 
     useEffect(() => {
         setActiveTab(capitalize(String(children.key.slice(1, -1))))
@@ -21,10 +22,12 @@ const Layout = ({ children }) => {
 
     return (
         <>
-            <Header />
-            {activeTab !== 'Home' && <PageTitle />}
-            {children}
-            <Footer />
+            <div className={styles.gradient}>
+                <Header />
+                {activeTab !== 'Home' && <PageTitle />}
+                {children}
+                <Footer />
+            </div>
         </>
     );
 }

e8f6be26482cda78bbef4350037e59145e28d1b5
LukaPodrug <lukapodra1@gmail.com>
2021-01-18 20:48:28 +0100

    input css

--- HCI-lab/src/components/searchTagBar/search/index.js
+++ HCI-lab/src/components/searchTagBar/search/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './style.module.css';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
 
 import _ from 'lodash';
 
@@ -11,7 +9,7 @@ const onType = ({ charCode, target: { value } }, searchAction, resetButtonAction
     if (charCode === 13) {
         wait = 0;
     }
-    
+
     if(value !== '') {
         resetButtonAction(true)
     }
@@ -24,7 +22,7 @@ const onType = ({ charCode, target: { value } }, searchAction, resetButtonAction
 
 const SearchBar = ({ action, resetButtonAction }) => {
     return (
-        <TextField id="#hackyInput" label="Search" type="text" variant="outlined" autocomplete="off" onKeyUp={(event) => onType(event, action, resetButtonAction)}/>
+        <input id="#hackyInput" className={styles.input} autoComplete="off" onKeyUp={(event) => onType(event, action, resetButtonAction)}/>
     );
 }
 
